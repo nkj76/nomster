3d4c3ab88d7b7d069e4fe321adeb50a8a1168b41
Niraj Jain <nirajkjain@gmail.com>
2019-04-05 04:52:02 +0000

    Nomster paginator added using the kaminari gem, needs more work

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,7 +2,9 @@ class PlacesController < ApplicationController
 
 
   def index
-    @places = Place.all
+    # @places = Place.all
+    @places = Place.all.page(params[:page]).per(6)
+    # @places = paginate_array(Place.all).page(params[:page])
   end
 
 
