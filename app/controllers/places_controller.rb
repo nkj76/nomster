f2bc024433c8c9986b6cd9c0893c6c7365855a70
Niraj Jain <nirajkjain@gmail.com>
2019-04-05 05:46:22 +0000

    added Bootstrap4 theme to kaminari, adjusted paginator font-size and top/bottom margins

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,9 +2,7 @@ class PlacesController < ApplicationController
 
 
   def index
-    # @places = Place.all
     @places = Place.all.page(params[:page]).per(6)
-    # @places = paginate_array(Place.all).page(params[:page])
   end
 
 
