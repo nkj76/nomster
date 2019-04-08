b3c68e8d1eb76f1f69ca1f4c116d71e98f325289
Niraj Jain <nirajkjain@gmail.com>
2019-04-08 18:09:29 +0000

    Lesson 27, validate that the place name is not blank

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -11,9 +11,12 @@ class PlacesController < ApplicationController
   end
 
   def create
-  #  Place.create(place_params)
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
@@ -35,7 +38,11 @@ class PlacesController < ApplicationController
       return render plain: 'Not Allowed', status: :forbidden
     end
     @place.update_attributes(place_params)
-    redirect_to root_path
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
   end
 
   def destroy
