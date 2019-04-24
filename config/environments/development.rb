af386abe05118f17e2b65abcad94dca2ef789bc5
Niraj Jain <nirajkjain@gmail.com>
2019-04-24 21:24:06 +0000

    Lesson 43, tested sending email automatically when a comment is added, then commented it out

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,17 +6,18 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
  
  # add config so that we can send email
- config.action_mailer.delivery_method = :smtp
+# config.action_mailer.delivery_method = :smtp
+config.action_mailer.delivery_method = :test
 
- config.action_mailer.smtp_settings = {
-  address: "smtp.gmail.com",
-  port: 587,
-  domain: "gmail.com",
-  authentication: "plain",
-  enable_starttls_auto: true,
-  user_name: ENV['GMAIL_ADDRESS'],
-  password: ENV['GMAIL_PASSWORD']
- }
+# config.action_mailer.smtp_settings = {
+#  address: "smtp.gmail.com",
+#  port: 587,
+#  domain: "gmail.com",
+#  authentication: "plain",
+#  enable_starttls_auto: true,
+#  user_name: ENV['GMAIL_ADDRESS'],
+#  password: ENV['GMAIL_PASSWORD']
+# }
 
   # add mailer info for 'devise' gem
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
