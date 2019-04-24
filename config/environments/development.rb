98c42e7b574ac8a851d1a842b2555d19a9c0b0a8
Niraj Jain <nirajkjain@gmail.com>
2019-04-24 17:47:37 +0000

    Lesson 41, got email working

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,6 +5,19 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
  
+ # add config so that we can send email
+ config.action_mailer.delivery_method = :smtp
+
+ config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV['GMAIL_ADDRESS'],
+  password: ENV['GMAIL_PASSWORD']
+ }
+
   # add mailer info for 'devise' gem
   config.action_mailer.default_url_options = { host: 'localhost:3030' }
 
@@ -35,7 +48,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
